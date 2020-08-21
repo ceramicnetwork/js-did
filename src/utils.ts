03e0738de9183a16af57363fd070f639071597d0
Joel Thorstensson <joel@oed.earth>
2020-08-21 23:06:06 +0200

    ref: refined createDagJWS api and added documentation

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,40 @@
 import dagCBOR from 'ipld-dag-cbor'
+import CID from 'cids'
 import base64url from 'base64url'
 
-interface EncodedPayload {
-  cid: string
-  linkedBlock: string
+export interface EncodedPayload {
+  cid: CID
+  linkedBlock: Uint8Array
 }
 
 export async function encodePayload(payload: Record<string, any>): Promise<EncodedPayload> {
   const block = dagCBOR.util.serialize(payload)
   return {
-    cid: base64url.encode(Buffer.from((await dagCBOR.util.cid(block)).bytes)),
-    linkedBlock: base64url.encode(block),
+    cid: await dagCBOR.util.cid(block),
+    linkedBlock: block,
+  }
+}
+
+export function u8aToBase64(bytes: Uint8Array): string {
+  return base64url.encode(Buffer.from(bytes))
+}
+
+interface JWSSignature {
+  protected: string
+  signature: string
+}
+
+export interface DagJWS {
+  payload: string
+  signatures: Array<JWSSignature>
+  link: CID
+}
+
+export function toDagJWS(jws: string, cid: CID): DagJWS {
+  const [protectedHeader, payload, signature] = jws.split('.')
+  return {
+    payload,
+    signatures: [{ protected: protectedHeader, signature }],
+    link: cid,
   }
 }
