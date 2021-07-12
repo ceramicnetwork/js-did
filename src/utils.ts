94be7007fcee004d17e5778c0c660f5f8d00b923
ukstv <sergey@ukstv.me>
2021-07-12 12:46:00 +0300

    feat: Pass issuer to verifyJWS

--- src/utils.ts
+++ src/utils.ts
@@ -30,3 +30,34 @@ export function fromDagJWS(jws: DagJWS): string {
   if (jws.signatures.length > 1) throw new Error('Cant convert to compact jws')
   return `${jws.signatures[0].protected}.${jws.payload}.${jws.signatures[0].signature}`
 }
+
+/**
+ * Make DID URL from DID and timestamp (= versionTime query)
+ */
+export function didWithTime(did: string, atTime?: number): string {
+  if (atTime) {
+    const versionTime = new Date(atTime).toISOString().split('.')[0] + 'Z'
+    return `${did}?versionTime=${versionTime}`
+  } else {
+    return did
+  }
+}
+
+/**
+ * `controller` field of DID Document can be one or multiple strings, if defined.
+ * Here we transform it into array of strings.
+ * Potentially it can be an empty array.
+ */
+export function extractControllers(
+  controllerProperty: string | Array<string> | undefined
+): Array<string> {
+  if (controllerProperty) {
+    if (Array.isArray(controllerProperty)) {
+      return controllerProperty
+    } else {
+      return [controllerProperty]
+    }
+  } else {
+    return []
+  }
+}
