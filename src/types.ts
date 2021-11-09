d60d2dd87fd983530370cb8c0fe37da77534ae27
stephhuynh18 <stephhuynh18@users.noreply.github.com>
2021-11-09 14:44:47 -0800

    Upgrade dag-jose-utils and use multiformats

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-import type CID from 'cids'
+import { CID } from 'multiformats/cid'
 import type { JWE } from 'did-jwt'
 import type { RPCClient, RPCConnection, RPCRequest, RPCResponse } from 'rpc-utils'
 
