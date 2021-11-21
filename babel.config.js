0435f1b03b7710a32b1712937c3b2508d28e9f21
Haardik <hhaardik@uwaterloo.ca>
2021-11-21 09:30:27 -0500

    bump did-jwt version to 5.11.1 (#51)
    * bump did-jwt version to 5.11.1
    
    * have babel target node 14
    
    * Update github action to use node 14

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 // babel.config.js
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 12 } }],
+    ['@babel/preset-env', { targets: { node: 14 } }],
     '@babel/preset-typescript',
   ],
 }
