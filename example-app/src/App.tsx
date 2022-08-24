a094e6bcffc4a0a03afabd99c2bf48184837877c
Artur Wdowiarski <arturwdowiarski@me.com>
2022-08-24 13:11:13 +0100

    Implemented simple authentication with Metamask

--- example-app/src/App.tsx
+++ example-app/src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { DIDSession } from 'did-session'
+import { EthereumAuthProvider } from '@ceramicnetwork/blockchain-utils-linking'
+import detectEthereumProvider from '@metamask/detect-provider'
 
 function App() {
   return (
@@ -10,8 +13,16 @@ function App() {
         <br/>
         <a
           className="App-link"
-          onClick={() => {
-            window.alert("Not implemented")
+          onClick={ async () => {
+            const ethProvider = await detectEthereumProvider();
+            // @ts-ignore
+            const addresses = await ethProvider.enable()
+            const authProvider = new EthereumAuthProvider(ethProvider, addresses[0])
+
+            // the resources param is a list of strings identifying resources you want to authorize for,
+            // according to the verification protocol you use (e.g. for Ceramic Network Protocol, these are ceramic stream IDs)
+            const session = await DIDSession.authorize(authProvider, { resources: ['resource']})
+            console.log('Session authorisation result', session)
           }}
         >
           Authenticate
