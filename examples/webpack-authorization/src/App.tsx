edcd55a7aaebc0ac0e77bf567eac28ce2bb1d979
Artur Wdowiarski <arturwdowiarski@me.com>
2022-08-24 13:13:05 +0100

    Moved the example examples folder

--- examples/webpack-authorization/src/App.tsx
+++ examples/webpack-authorization/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
             console.log('Session authorisation result', session)
           }}
         >
-          Authenticate
+          Authenticate & Authorize
         </a>
       </header>
     </div>
