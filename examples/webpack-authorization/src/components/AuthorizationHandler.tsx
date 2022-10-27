8faf5507e4e771ec437f7a0481e26c00d1ff5f79
Artur Wdowiarski <arturwdowiarski@me.com>
2022-10-27 08:36:30 +0100

    Simplified the authorization example app

--- examples/webpack-authorization/src/components/AuthorizationHandler.tsx
+++ examples/webpack-authorization/src/components/AuthorizationHandler.tsx
@@ -6,50 +6,6 @@ import React, { useState } from 'react'
 function AuthorizationHandler() {
   const [session, setSession] = useState<DIDSession>()
 
-  const renderUnauthenticated = () => {
-    return (
-      <label>
-        There's no authorized session, click the 'Authenticate & Authorize' button
-      </label>
-    )
-  }
-
-  const renderAuthenticated = () => {
-    return (
-      <div>
-        <label> Authenticated DID: {session!.did.id} </label>
-        <br/>
-        <label> session.hasSession: {session!.hasSession ? 'true' : 'false'} </label>
-        <br/>
-        <label> session.isExpired: {session!.isExpired ? 'true' : 'false'} </label>
-        <br/>
-        <label> session.authorizations: {session!.authorizations} </label>
-        <br/>
-        <label> session.expiresInSecs: {session!.expireInSecs} </label>
-        <br/>
-        <a
-          className="App-link"
-          onClick={ () => {
-            window.alert(`Serialized Session: ${session!.serialize()}`)
-          }}
-        >
-          Serialize Session
-        </a>
-        <br/>
-        <a
-          className="App-link"
-          onClick={ async () => {
-            const serialized = session!.serialize()
-            const newFromSerialized = await DIDSession.fromSession(serialized)
-            window.alert(`Serialized Session: ${serialized}; New Copy from Serialized Session: ${newFromSerialized}`)
-          }}
-        >
-          Serialize and Create a Copy
-        </a>
-      </div>
-    )
-  }
-
   const authenticate = async () => {
     const ethProvider = await detectEthereumProvider();
     const addresses = await (window.ethereum as any).request({ method: 'eth_requestAccounts' })
@@ -57,30 +13,81 @@ function AuthorizationHandler() {
 
     const oneWeek = 60 * 60 * 24 * 7
 
-    // the resources param is a list of strings identifying resources you want to authorize for,
-    // according to the verification protocol you use (e.g. for Ceramic Network Protocol, these are ceramic stream IDs)
     const session = await DIDSession.authorize(
       authProvider,
       {
-        resources: ['test-resource'],
+        resources: ['test-resource', 'another-test-resource'],
         expiresInSecs: oneWeek,
         domain: 'YourAppName'
       })
     setSession(session)
   }
 
-  return (
-    <div>
+  const render = () => {
+    return (
+      <div>
+        { session === undefined || !session.isAuthorized() ? renderUnauthenticated() : renderAuthenticated() }
+      </div>
+    )
+  }
+
+  const renderUnauthenticated = () => {
+    return (
       <a
         className="App-link"
         onClick={authenticate}
       >
         Authenticate & Authorize
       </a>
-      { session === undefined || !session.isAuthorized() ? renderUnauthenticated() : renderAuthenticated()
-      }
-    </div>
-  )
+    )
+  }
+
+  const renderAuthenticated = () => {
+    return (
+      <div>
+        <a
+          className="App-link"
+          onClick={ () => {
+            console.log(`${JSON.stringify(session?.cacao, null, 2)}`)
+          }}
+        >
+          Log session's CACAO
+        </a>
+        <br/>
+        <a
+          className="App-link"
+          onClick={ () => {
+            console.log(`${session!.serialize()}`)
+          }}
+        >
+          Log serialized session
+        </a>
+        <br/>
+        <input type="text" id="message" name="message"/>
+        <a
+          className="App-link"
+          onClick={ async () => {
+            const message = (document?.getElementById("message") as HTMLInputElement).value
+            const signed = await session!.did.createJWS(message)
+            console.log(`${signed.signatures[0].signature}`)
+          }}
+        >
+          Log message's signature
+        </a>
+        <br/>
+        <a
+          className="App-link"
+          onClick={ () => {
+            setSession(undefined)
+          }}
+        >
+          Sign out
+        </a>
+      </div>
+    )
+  }
+
+  return render()
 }
 
 export default AuthorizationHandler
