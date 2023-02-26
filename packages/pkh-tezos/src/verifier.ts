e3a1f2bb62ba93efb01d80a3937144fdddce05ac
Zach Ferland <zachferland@gmail.com>
2023-02-26 11:15:24 -0500

    feat(pkh-tezos): remove @taquito/utils for stablelib (#147)
    * feat(pkh-tezos): remove @taquito/utils for stablelib
    
    * build(pkh-tezos): add minimal sign/verify tests
    
    * fix(pkh-tezos): pk -> address encoding fix, base58btc vs bs58check
    
    * fix(pkh-tezos): fix sig prefix handling
    
    * chore(pkh-tezos): bump version
    
    ---------
    
    Co-authored-by: Joel Thorstensson <joel@3box.io>

--- packages/pkh-tezos/src/verifier.ts
+++ packages/pkh-tezos/src/verifier.ts
@@ -1,4 +1,3 @@
-import { verifySignature, getPkhfromPk } from '@taquito/utils'
 import {
   SiwTezosMessage,
   Cacao,
@@ -8,6 +7,18 @@ import {
   Verifiers,
 } from '@didtools/cacao'
 import { AccountId } from 'caip'
+import * as u8a from 'uint8arrays'
+import { hash } from '@stablelib/blake2b'
+import { hash as sha256 } from '@stablelib/sha256'
+import { verify } from '@stablelib/ed25519'
+
+// ED
+const TZ1Prefix = new Uint8Array([6, 161, 159])
+const TZ1Length = 20
+const EDPKPrefix = new Uint8Array([13, 15, 37, 217])
+const EDSIGPrefix = new Uint8Array([9, 245, 205, 134, 18])
+const SIGPrefix =  new Uint8Array([4, 130, 43])
+const Base58CheckSumLength = 4
 
 export function getTezosVerifier(): Verifiers {
   return {
@@ -16,6 +27,64 @@ export function getTezosVerifier(): Verifiers {
       verifyTezosSignature(cacao, opts)
     },
   }
+}
+
+export function getPkhfromPk(publicKey: string): string {
+  const pkPrefix = publicKey.substring(0, 4)
+  if (pkPrefix !== 'edpk')
+    throw new Error('Tezos Signature type not supported, only type tezos:ed25519')
+  const decoded = b58cdecode(publicKey, EDPKPrefix)
+  const hashed = hash(decoded, TZ1Length)
+  const result = b58cencode(hashed, TZ1Prefix)
+  return result
+}
+
+function verifyEdSignature(
+  decodedSig: Uint8Array,
+  bytesHash: Uint8Array,
+  decodedPublicKey: Uint8Array
+) {
+  try {
+    return verify(decodedPublicKey, bytesHash, decodedSig)
+  } catch (e) {
+    return false
+  }
+}
+
+//bs58btc decoding, bs58check - checksum
+function b58cdecode(enc: string, prefixArg: Uint8Array): Uint8Array {
+  const u8akey = u8a.fromString(enc, 'base58btc')
+  return u8akey.slice(prefixArg.length, u8akey.length - Base58CheckSumLength)
+}
+
+//bs58check encoding, bs58btc + checksum
+function b58cencode(value: Uint8Array, prefix: Uint8Array) {
+  const n = new Uint8Array(prefix.length + value.length)
+  n.set(prefix)
+  n.set(value, prefix.length)
+  const checksum = getCheckSum(n)
+  const nc =  new Uint8Array(n.length + 4)
+  nc.set(n)
+  nc.set(checksum, prefix.length + value.length)  
+  return u8a.toString(nc, 'base58btc')
+}
+
+function getCheckSum(u8a: Uint8Array): Uint8Array {
+  const hashed = sha256(sha256(u8a))
+  return hashed.slice(0,4)
+}
+
+export function verifySignature(payload: string, publicKey: string, signature: string): boolean {
+  const pkPrefix = publicKey.substring(0, 4)
+  const sigPrefix = signature.startsWith('sig') ? signature.substr(0, 3) : signature.substr(0, 5)
+
+  if (pkPrefix !== 'edpk' || !(sigPrefix === 'edsig' || sigPrefix === 'sig'))
+    throw new Error('Tezos Signature type not supported, only type tezos:ed25519')
+
+  const decodedPublicKey = b58cdecode(publicKey, EDPKPrefix)
+  const decodedSig = b58cdecode(signature, sigPrefix === 'edsig' ? EDSIGPrefix :  SIGPrefix )
+  const bytesHash = hash(u8a.fromString(payload, 'base16'), 32)
+  return verifyEdSignature(decodedSig, bytesHash, decodedPublicKey)
 }
 
 export function verifyTezosSignature(cacao: Cacao, options: VerifyOptions) {
--- packages/pkh-tezos/test/index.test.ts
+++ packages/pkh-tezos/test/index.test.ts
@@ -1,5 +1,28 @@
-describe('Solana Cacao Auth Verify', () => {
-  test.skip('Verify', () => {
+import { InMemorySigner } from '@taquito/signer'
+import { verifySignature, getPkhfromPk } from '../src/verifier'
+import { char2Bytes, verifySignature as taquitoVerifySignature, getPkhfromPk as taquitoGetPkhfromPk } from '@taquito/utils'
 
+describe('Tezos Cacao Auth Verify', () => {
+
+  const signer = new InMemorySigner(
+    'edskS3DtVSbWbPD1yviMGebjYwWJtruMjDcfAZsH9uba22EzKeYhmQkkraFosFETmEMfFNVcDYQ5QbFerj9ozDKroXZ6mb5oxV'
+  )
+  test('Signer Verify', async () => {
+    const signed = await signer.sign(char2Bytes('hello'))
+    const pubkey = await signer.publicKey()
+
+    const taquitoRes = taquitoVerifySignature(char2Bytes('hello'), pubkey, signed.prefixSig)
+    expect(taquitoRes).toBeTruthy()
+
+    const res = verifySignature(char2Bytes('hello'), pubkey, signed.sig)
+    expect(res).toBeTruthy()
+
+    const resPrefix = verifySignature(char2Bytes('hello'), pubkey,  signed.prefixSig)
+    expect(resPrefix).toBeTruthy()
+  })
+
+  test('PKH (tezos address) from PK', async () => {
+    const pubkey = await signer.publicKey()
+    expect(taquitoGetPkhfromPk(pubkey)).toEqual(getPkhfromPk(pubkey))
   })
 })
