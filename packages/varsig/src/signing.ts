43685a0ae330768353a2286bd3d180c7eacf431c
Sergey Ukustov <sergey@ukstv.me>
2023-11-21 16:17:00 +0300

    wip

--- packages/varsig/src/signing.ts
+++ packages/varsig/src/signing.ts
@@ -1,5 +1,8 @@
+import { BytesTape } from './bytes-tape.js'
+import { UnreacheableCaseError } from './unreachable-case-error'
+
 export enum SigningKind {
-  RSASSA_PKCS1_v1_5 = 0x1205,
+  RSA = 0x1205,
   SECP256K1 = 0xe7,
 }
 
@@ -9,3 +12,45 @@ export type SigningSecp256k1 = {
 }
 
 export type SigningAlgo = SigningSecp256k1
+
+export class SigningDecoder {
+  constructor(private readonly tape: BytesTape) {}
+
+  static read(tape: BytesTape): SigningAlgo {
+    return new SigningDecoder(tape).read()
+  }
+
+  read(): SigningAlgo {
+    const signingSigil = this.tape.readVarint<SigningKind>()
+    switch (signingSigil) {
+      case SigningKind.SECP256K1: {
+        const recoveryBit = this.tape.readVarint()
+        if (!(recoveryBit === 27 || recoveryBit === 28)) {
+          throw new Error(`Wrong recovery bit`)
+        }
+        return {
+          kind: SigningKind.SECP256K1,
+          recoveryBit: recoveryBit || undefined,
+        }
+      }
+      case SigningKind.RSA:
+        throw new Error(`Not implemented: signingSigil: RSA`)
+      default:
+        throw new UnreacheableCaseError(signingSigil, 'signing kind')
+    }
+  }
+
+  readSignature(signing: SigningAlgo): Uint8Array {
+    switch (signing.kind) {
+      case SigningKind.SECP256K1: {
+        if (signing.recoveryBit) {
+          return this.tape.read(65)
+        } else {
+          return this.tape.read(64)
+        }
+      }
+      default:
+        throw new UnreacheableCaseError(signing.kind, 'signing kind')
+    }
+  }
+}
