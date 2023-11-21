9e54144595af761c97585b4f453949cd59952f4b
Sergey Ukustov <sergey@ukstv.me>
2023-11-21 16:07:51 +0300

    wip

--- packages/varsig/src/at0.ts
+++ packages/varsig/src/at0.ts
@@ -9,23 +9,7 @@ export enum HASHING {
   KECCAK256 = 0x1b,
 }
 
-export type SigningSECP256K1 = {
-  kind: SIGNING.SECP256K1
-  recoveryBit: undefined | 27 | 28
-}
-
 export enum CANONICALIZATION {
   EIP712 = 0xe712,
   EIP191 = 0xe191,
-}
-
-export type CanonicalizationEIP191 = {
-  kind: CANONICALIZATION.EIP191
-}
-
-export type Varsig<TSigniing, TCanonicalization> = {
-  signing: TSigniing
-  hashing: HASHING
-  canonicalization: TCanonicalization
-  signature: Uint8Array
 }
